dc79f273484f4b699e7632f9c4c62c4022425811
Mottacvh <continmotta123@gmail.com>
2026-06-18 16:50:06 -0300

    Upando Arquivos

---
+++ src/main/java/br/com/tecloja/api/TeclojaApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.tecloja.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeclojaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeclojaApiApplication.class, args);
+    }
+}
